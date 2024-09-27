2ea6272ba26d9920dfc3861a423dff26f71c49bb
Usjid Nisar <usjidn@gmail.com>
2024-09-27 22:45:20 +0500

    Initial Commit

---
+++ backend/src/whenwecome/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.contrib import admin
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="WhenWeCome API",
+        default_version='v1',
+        description="API documentation for WhenWeCome",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="usjidn@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+]
